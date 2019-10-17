c5578c7b6aeac155e48e1e281ca26584c271a496
Pursuit <pursuit@Pursuits-MacBook-Pro-3.local>
2019-10-17 18:10:04 -0400

    Created Main VC and Favorites VC, created Persistence Manager and dragged outlets

--- WeatherProject/WeatherProject/ViewController.swift
+++ WeatherProject/WeatherProject/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    @IBOutlet weak var zipcodeInputText: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ WeatherProject/WeatherProject/WeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCell.swift
+//  WeatherProject
+//
+//  Created by Pursuit on 10/17/19.
+//  Copyright © 2019 Neema Philippe. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+    
+    @IBOutlet weak var forecastImage: UIImageView!
+    
+    
+}
